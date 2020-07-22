f8f2aca5174155669e167887934f18c54f9da2cb
vineetsemwal <vineetsemwal82@gmail.com>
2020-07-22 15:16:41 +0530

    wrapper examples

---
+++ src/BooleanWrapper.java
@@ -0,0 +1,14 @@
+
+public class BooleanWrapper {
+
+	public static void main(String[]args) {		
+		boolean bool=false;// primitive
+		Boolean boolWrapper=new Boolean(true);//wrapper
+		boolean primitiveBoolean=boolWrapper.booleanValue();
+		String boolString=boolWrapper.toString();
+		System.out.println("wrapper="+boolWrapper);
+		System.out.println("primitive="+primitiveBoolean);
+		System.out.println("string="+boolString);
+	}
+	
+}
---
+++ src/IntegerWrapperExample.java
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * int , Integer  
+  long,  Long
+  float, Float
+  double, Double
+  boolean, Boolean
+
+ */
+public class IntegerWrapperExample {
+
+	public static void main(String args[]) {
+		
+			
+		// int is primitive type
+		int i1=10;		// 4 byte , 32 bits
+		// Integer is wrapper class, advantage of wrapper class is we can reuse 
+		// predefined methods available in the Integer class
+		Integer i2=new Integer(10); 
+		int primitivei2=i2.intValue();
+		byte b=i2.byteValue();// -128 to 127
+		String str=i2.toString();// "10"
+		double doubleValue=i2.doubleValue();//10.00
+		float floatValue=i2.floatValue();// 10.00
+		long longValue=i2.longValue();// 8 bytes, 64 bits
+		
+		System.out.println("integer="+i2);
+		System.out.println("string="+str);
+		System.out.println("double="+doubleValue);
+		
+		
+		
+	}
+	
+}
