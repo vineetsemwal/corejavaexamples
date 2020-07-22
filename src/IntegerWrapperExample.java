2cb3488688a2e8809379e40b405c527adccd6e6b
vineetsemwal <vineetsemwal82@gmail.com>
2020-07-22 16:20:30 +0530

    wrapper examples

--- src/IntegerWrapperExample.java
+++ src/IntegerWrapperExample.java
@@ -10,11 +10,23 @@
  */
 public class IntegerWrapperExample {
 
-	public static void main(String args[]) {				
+	public static void main(String args[]) {			
+	    //LHS is Integer wrapper reference , RHS is int
+		// should not be possible
+		Integer i3= 10;
+	   // Integer i3=new Integer(10);// Auto Boxing
+		
+		//primitive is in LHS, wrapper Object in RHS
+		int i4 =new Integer(10); // Auto unboxing
+		//int i4= object.intValue();
+		
+		
+		
 		// int is primitive type
 		int i1=10;		// 4 byte , 32 bits
 		// Integer is wrapper class, advantage of wrapper class is we can reuse 
 		// predefined methods available in the Integer class
+				
 		Integer i2=new Integer(10); 
 		int primitivei2=i2.intValue();
 		byte b=i2.byteValue();// -128 to 127
@@ -22,7 +34,7 @@ public class IntegerWrapperExample {
 		double doubleValue=i2.doubleValue();//10.00
 		float floatValue=i2.floatValue();// 10.00
 		long longValue=i2.longValue();// 8 bytes, 64 bits
-		
+				         
 		System.out.println("integer="+i2);
 		System.out.println("string="+str);
 		System.out.println("double="+doubleValue);	
---
+++ src/demos/DoubleWrapperExample.java
@@ -0,0 +1,20 @@
+package demos;
+
+public class DoubleWrapperExample {
+
+	public static void main(String[]args) {
+	Double wrapper=new Double(10.2);
+	double convertedToPrimitive=wrapper.doubleValue();
+    wrapper=11.3;
+    // wrapper=new Double(11.3);
+    
+    double primitive=10.2;
+    primitive=new Double(12.2);
+    // primitive= doubleObject.doubleValue()
+    
+       
+    
+    
+	}
+	
+}
