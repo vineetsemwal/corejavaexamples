ef9a01906b8c8fc7875099b9d16a8e7891762414
vineetsemwal <vineetsemwal82@gmail.com>
2020-07-25 16:36:24 +0530

    added comments

--- src/collectiondemos/ListExample.java
+++ src/collectiondemos/ListExample.java
@@ -16,32 +16,37 @@ import java.util.*;
 2) Ordered Collection
 
 3) Elements can be duplicated
-
+ 
+ arraylist is good in performance fetching element by index 
+ arraylist is bad in performance when elements are added or removed at start or middle
  *
  */
 public class ListExample {
 	
-	public static void main(String[]args) {
-     List list=new ArrayList();	
-	 list.add("prakash");
+	public static void main(String[]args) {	  	
+     List list=new ArrayList();// Object arr[];	
+	 list.add("prakash");//adding element at the end
 	 list.add("pradeep");
 	 list.add("anuj");
 	 list.add("ankit");
 	 list.add("pradeep");
 	 
-	 Object element2=list.get(2);
+	 Object element2=list.get(2);// fetching element at index 2
 	 String element2String=(String)element2;
 	 int size=list.size();
 		 
 	 System.out.println("size before removing element="+size);
 	 
-	 list.remove(1);
+	 list.remove(1);//removing element at index 1
 	 
-	 size=list.size();
+	 size=list.size();//size of list
 	
-	 //list.clear();
+	 //list.clear();// clearing list
 	 
 	 System.out.println("size after removing element="+size);
+	 
+	 boolean iscontain=list.contains("anuj");
+	 System.out.println("contains="+iscontain);
 	 
 	 for(int i=0;i<list.size();i++) {
 		Object obj =list.get(i);
