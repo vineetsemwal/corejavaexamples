c9d2cab947a1830daba4255c0d8f9fd2d6aef307
vineetsemwal <vineetsemwal82@gmail.com>
2020-07-28 18:00:05 +0530

    different threadpools added

--- src/multithreaddemos/ExecutorExample.java
+++ src/multithreaddemos/ExecutorExample.java
@@ -14,19 +14,25 @@ public class ExecutorExample {
      *
      *
      *   3 tasks , 2 threads
+     *   
+     *   Different Thread pools
+     *   1) Fixed thread pool
+     *   2) Cached thread pool : threads can grow as per requirement
+     *   3) Single thread pool : only 1 thread is there
      */
     public static void main(String[]args){
-       ExecutorService executorService =Executors.newFixedThreadPool(2);
+    // ExecutorService executorService =Executors.newFixedThreadPool(2);
+    	
+       //ExecutorService executorService =Executors.newCachedThreadPool();
+    	ExecutorService executorService =Executors.newSingleThreadExecutor();
+        
        Runnable task1=new NumberRunnable("task 1");
        Runnable task2=new NumberRunnable("task 2");
        Runnable task3=new NumberRunnable("task 3");
        executorService.submit(task1);
        executorService.submit(task2);
        executorService.submit(task3);
-
-
-
-        executorService.shutdown();
+       executorService.shutdown();
 
     }
 
