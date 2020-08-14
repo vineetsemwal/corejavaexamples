28394e3c72f51586a153066f44db9fb08a504e1b
vineetsemwal <vineetsemwal82@gmail.com>
2020-08-14 17:13:23 +0530

    mvc demo improved

--- bootmvc/src/main/java/com/dxctraining/bootmvc/EmployeeController.java
+++ bootmvc/src/main/java/com/dxctraining/bootmvc/EmployeeController.java
@@ -1,11 +1,61 @@
 package com.dxctraining.bootmvc;
 
+import com.dxctraining.bootmvc.entities.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.PostConstruct;
+import java.util.*;
 
 @Controller
 public class EmployeeController {
+
+    private Map<Integer, Employee> store =new HashMap<>();
+
+    @PostConstruct
+    public void init(){
+        Employee employee1=new Employee("vineel",21,5000);
+        employee1.setId(1);
+        store.put(1,employee1);
+
+        Employee employee2=new Employee("fazil",22,3000);
+        employee2.setId(2);
+        store.put(2,employee2);
+
+    }
+
+
+
+    @GetMapping("/profile")
+    public ModelAndView employeeDetails(@RequestParam("id")int id){
+       Employee emp=findEmployeeById(id);
+       // details is name of view
+        // employee is name of model object
+        // emp is model data
+       ModelAndView modelAndView=new ModelAndView("details","employee",emp);
+       return modelAndView;
+    }
+
+    @GetMapping("/listall")
+    public ModelAndView all(){
+       Collection<Employee>values=findAll();
+        ModelAndView modelAndView=new ModelAndView("list","employees",values);
+        return modelAndView;
+    }
+
+
+    public Employee findEmployeeById(int id){
+       Employee employee= store.get(id);
+       return employee;
+    }
+
+
+    public Collection<Employee>findAll() {
+        Collection<Employee> values = store.values();
+        return values;
+    }
 
 
 
--- bootmvc/src/main/java/com/dxctraining/bootmvc/HelloController.java
+++ bootmvc/src/main/java/com/dxctraining/bootmvc/HelloController.java
@@ -14,10 +14,13 @@ public class HelloController {
      *  1) decides which view should render response
      *  2)model data to the view
      */
-  @GetMapping("/hello")
+  @GetMapping("/hellomsg")
   public ModelAndView  helloworld(){
       System.out.println("inside helloworld");
       String msg="hello friend";
+      // hello is name of the view
+      // message is model object name
+      // msg is model object value
       ModelAndView modelAndView=new ModelAndView("hello","message",msg);
       return modelAndView;
   }
