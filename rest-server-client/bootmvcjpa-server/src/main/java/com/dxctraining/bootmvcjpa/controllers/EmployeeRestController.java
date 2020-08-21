508962a8839b6762a72fedafe953e16233524ece
vineetsemwal <vineetsemwal82@gmail.com>
2020-08-21 11:06:08 +0530

    rest server improved

--- rest-server-client/bootmvcjpa-server/src/main/java/com/dxctraining/bootmvcjpa/controllers/EmployeeRestController.java
+++ rest-server-client/bootmvcjpa-server/src/main/java/com/dxctraining/bootmvcjpa/controllers/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package com.dxctraining.bootmvcjpa.controllers;
 
 import com.dxctraining.bootmvcjpa.dto.CreateEmployeeRequest;
+import com.dxctraining.bootmvcjpa.dto.EmployeeDto;
 import com.dxctraining.bootmvcjpa.dto.UpdateEmployeeRequest;
 import com.dxctraining.bootmvcjpa.entities.Employee;
 import com.dxctraining.bootmvcjpa.exceptions.EmployeeNotFoundException;
@@ -10,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -23,41 +26,46 @@ public class EmployeeRestController {
       uri is /employees/add
       url http://localhost:8585/employees/add
    */
-    @PostMapping("/add")
+    @PostMapping(value = "/add")
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee create(@RequestBody CreateEmployeeRequest requestData) {
+    public EmployeeDto create(@RequestBody CreateEmployeeRequest requestData) {
         String name = requestData.getName();
         String password = requestData.getPassword();
         int age = requestData.getAge();
         double salary = requestData.getSalary();
         Employee employee = new Employee(name, password, age, salary);
         employee = service.save(employee);
-        return employee;
+        EmployeeDto response=employeeDto(employee);
+        return response;
     }
+
+
 
     /*
     uri is /employees/get
-     /employees/get?id=10
-    /employees/get/abc/10
+   request parameter  /employees/get?id=10
+   path variable /employees/get/10
      */
     @GetMapping("/get/{id}")
-    public Employee findEmployee(@PathVariable("id") int id) {
+    public EmployeeDto findEmployee(@PathVariable("id") int id) {
         Employee employee = service.findEmployeeById(id);
-        return employee;
+        EmployeeDto response=employeeDto(employee);
+        return response;
     }
 
 
     @GetMapping
-    public List<Employee>fetchAll(){
+    public List<EmployeeDto>fetchAll(){
        List<Employee>list= service.allEmployees();
-       return list;
+      List<EmployeeDto>response= employeeDto(list);
+       return response;
     }
 
     /**
      * uri is /employees/update
      */
     @PutMapping("/update")
-    public Employee updateEmployee(@RequestBody UpdateEmployeeRequest requestData) {
+    public EmployeeDto updateEmployee(@RequestBody UpdateEmployeeRequest requestData) {
         String name = requestData.getName();
         String password = requestData.getPassword();
         int age = requestData.getAge();
@@ -66,7 +74,8 @@ public class EmployeeRestController {
         Employee employee = new Employee(name, password, age, salary);
         employee.setId(id);
         employee = service.update(employee);
-        return employee;
+        EmployeeDto response=employeeDto(employee);
+        return response;
     }
 
     @GetMapping("/authenticate/{id}/{password}")
@@ -75,6 +84,20 @@ public class EmployeeRestController {
        return result;
     }
 
+
+    public EmployeeDto employeeDto(Employee employee){
+        EmployeeDto dto=new EmployeeDto(employee.getId(),employee.getName(),employee.getPassword(),employee.getSalary(),employee.getAge());
+        return dto;
+    }
+
+    public List<EmployeeDto>employeeDto(List<Employee>employees){
+        List<EmployeeDto>dtos=new ArrayList<>();
+        for (Employee employee:employees){
+            EmployeeDto dto=employeeDto(employee);
+            dtos.add(dto);
+        }
+        return dtos;
+    }
 
 }
 
