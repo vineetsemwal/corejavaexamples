225e6e0a1fcc92d3b6ed66ac36291b5f5022145f
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-02 12:58:12 +0530

    controller added for jms demo

---
+++ jmsexperiments/jmsexperiment1/src/main/java/com/dxctraining/jmsexperiment1/MessageController.java
@@ -0,0 +1,37 @@
+package com.dxctraining.jmsexperiment1;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/messages")
+@RestController
+public class MessageController {
+
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @PostMapping("/add/mp")
+    public String sendForMp(@RequestBody SimpleMessage message){
+       jmsTemplate.convertAndSend("mp",message);
+       return "msg sent for consumption";
+    }
+
+    @PostMapping("/add/telangana")
+    public String sendForTelanagna(@RequestBody SimpleMessage message){
+        jmsTemplate.convertAndSend("telangana",message);
+        return "msg sent for consumption";
+
+    }
+
+    @PostMapping("/add/ap")
+    public String sendForAndhra(@RequestBody SimpleMessage message){
+        jmsTemplate.convertAndSend("andhra",message);
+        return "msg sent for consumption";
+    }
+
+
+}
