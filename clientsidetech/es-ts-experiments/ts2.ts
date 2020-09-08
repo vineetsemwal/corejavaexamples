90044267f76f2e482ac8d75aef5e8a8fdd6e3472
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-08 17:58:14 +0530

    new ts example

---
+++ clientsidetech/es-ts-experiments/ts2.ts
@@ -0,0 +1,39 @@
+class Student{
+    name:string;
+    age:number;    
+    constructor(name:string, age:number){
+     this.name=name;
+     this.age=age;
+    }
+
+    display(){
+        console.log("student="+this.name+" "+this.age);
+    }
+
+}
+
+class ITStudent extends Student{
+    allotted:boolean;
+   
+    constructor(name:string, age:number, allotted:boolean){
+        super(name, age);
+        this.allotted=allotted;
+    }
+
+    display(){
+        super.display();
+        console.log("computer alloted="+this.allotted);
+    }
+
+}
+
+
+ let student1:Student=new ITStudent("bhanu",21,true);
+ let student2:ITStudent=new ITStudent("dharma",21,true);
+
+  let students=[student1,student2];
+
+   for(let student of students){
+       student.display();
+   }
+
