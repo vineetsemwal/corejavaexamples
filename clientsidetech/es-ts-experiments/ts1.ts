fc6554c085eae221b73b8a9b50cde486c01ffe78
vineetsemwal <vineetsemwal82@gmail.com>
2020-09-08 17:39:04 +0530

    typescript example added

---
+++ clientsidetech/es-ts-experiments/ts1.ts
@@ -0,0 +1,16 @@
+let num1 : number=10;
+let str: string="hello";
+let flag:boolean=true;
+
+console.log("num1="+num1);
+console.log("str="+str);
+console.log("flag="+flag);
+
+function add(num1 :number , num2 : number):number{
+
+    let result:number=num1+num2;
+     return result;
+}
+
+ let result:number=add(1,2);
+ console.log("result="+result);
